b14c27af2fbcc80cbffbaa8dd40c8d383072352d
guaosi <guaosi@vip.qq.com>
2018-07-20 18:02:31 +0800

    'index-recommend'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,22 @@
         <home-header></home-header>
         <home-swiper></home-swiper>
         <home-icon></home-icon>
+        <home-recommend></home-recommend>
+
     </div>
 </template>
 <script>
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/HomeSwiper'
 import HomeIcon from './components/HomeIcon'
+import HomeRecommend from './components/HomeRecommend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcon
+    HomeIcon,
+    HomeRecommend
   }
 }
 </script>
