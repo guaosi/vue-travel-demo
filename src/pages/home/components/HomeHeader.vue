605541f0170b4f992fbb081210899fb7dad3ce1e
guaosi <guaosi@vip.qq.com>
2018-07-21 18:57:07 +0800

    'city-router'

--- src/pages/home/components/HomeHeader.vue
+++ src/pages/home/components/HomeHeader.vue
@@ -2,15 +2,17 @@
     <div class='header'>
         <div class='header-left'>
             <span class="iconfont back-icon">&#xe624;</span>
-            </div>
+        </div>
         <div class='header-input'>
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
             </div>
-        <div class='header-right'>
-            {{city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+          <div class='header-right'>
+              {{city}}
+              <span class="iconfont arrow-icon">&#xe64a;</span>
+          </div>
+        </router-link>
     </div>
 </template>
 <script>
@@ -29,7 +31,7 @@ export default {
 // 这里给的图是原来尺寸的2倍
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -53,6 +55,7 @@ export default {
       width: 1.24rem
       float: right
       text-align:center
+      color: #fff
       .arrow-icon
         margin-left: -0.04rem
         font-size: .24rem
