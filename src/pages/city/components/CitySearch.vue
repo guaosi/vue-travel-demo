5a075cf56d2ea5bbe370abcc5d77abf5ccafba0e
guaosi <guaosi@vip.qq.com>
2018-07-21 19:43:57 +0800

    'city-search'

---
+++ src/pages/city/components/CitySearch.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" name="" placeholder="输入城市名或拼音">
+    </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang='stylus' scoped>
+  @import '~style/variable.styl';
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-heigght: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
