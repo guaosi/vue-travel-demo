6ef9a8d8201f5f20ac560addea61fdebee1efa28
guaosi <guaosi@vip.qq.com>
2018-07-23 16:55:49 +0800

    'detail-banner'

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container" @click="handleCloseBanner">
+     <div class='wrapper'>
+         <swiper :options="swiperOption">
+    <!-- slides -->
+            <swiper-slide v-for="(item, index) in imgs" :key="index">
+             <img class="gallary-img" :src="item">
+            </swiper-slide>
+    <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+         </swiper>
+     </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleCloseBanner () {
+      this.$emit('close')
+    }
+  },
+  data: function () {
+    return {
+    //  官网上的配置,用来显示下面的滚动点
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        // 控制分页格式
+        paginationType: 'fraction',
+        // 因为swiper隐藏的时候width会有问题，所以需要observer
+        // ovserver可以监听关于swiper的dom的改变,然后刷新
+        observeParents: true,
+        observer: true
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+      height: 0
+      width: 100%
+      padding-bottom: 100%
+      .gallary-img
+        width: 100%
+      .swiper-pagination
+        color: #fff
+        bottom: -1rem
+</style>
---
+++ src/pages/detail/components/DetailBanner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleClickBanner">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1412/43/1217ccf8106bca9a077b58bf56bb2598.water.jpg_600x330_e71586ed.jpg">
+      <div class="banner-info">
+        <div class="banner-tittle">
+            三亚宋城千古情景区
+        </div>
+        <div class="banner-number">
+            <span class="iconfont banner-icon">&#xe692;</span>
+            39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showBanner" @close="closeBanner"></common-gallary>
+  </div>
+</template>
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleClickBanner () {
+      this.showBanner = true
+    },
+    closeBanner () {
+      this.showBanner = false
+    }
+  },
+  data () {
+    return {
+      showBanner: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1411/e3/13b35b267d8d16b0d9694683e4a991b0.water.jpg_r_800x800_ff12e8fa.jpg', 'http://img1.qunarzz.com/sight/p0/1411/26/2fde4ddb0af5e1bee83a50efaebc96cb.water.jpg_r_800x800_ae3620b3.jpg', 'http://img1.qunarzz.com/sight/p0/1411/94/011cc6d81dbaadfc0f332662eb972c08.water.jpg_r_800x800_f4e3a406.jpg'
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
