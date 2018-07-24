6fc114f78b4585f83279f850865573a826d78ec3
guaosi <guaosi@vip.qq.com>
2018-07-24 14:16:03 +0800

    'detail-listr'

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,16 +2,40 @@
     <div class="content">
       <detail-banner></detail-banner>
       <detail-header></detail-header>
+      <detail-list :list="categoryList"></detail-list>
     </div>
 </template>
 <script>
 import DetailBanner from './components/DetailBanner'
 import DetailHeader from './components/DetailHeader'
+import DetailList from './components/DetailList'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      categoryList: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
